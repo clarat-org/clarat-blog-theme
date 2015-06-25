87346c17337fbd2f4859bea71a9026821e56b5db
mhe <mail@marcus-herrmann.com>
2015-06-25 11:41:07 +0200

    For #331

--- templates/head.php
+++ templates/head.php
@@ -7,6 +7,24 @@
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+    <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/apple-touch-icon-57x57.png" rel="apple-touch-icon" sizes="57x57" />
+    <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/apple-touch-icon-60x60.png" rel="apple-touch-icon" sizes="60x60" />
+    <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72" />
+    <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/apple-touch-icon-76x76.png" rel="apple-touch-icon" sizes="76x76" />
+    <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114" />
+    <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/apple-touch-icon-120x120.png" rel="apple-touch-icon" sizes="120x120" />
+    <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144" />
+    <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/apple-touch-icon-152x152.png" rel="apple-touch-icon" sizes="152x152" />
+    <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/apple-touch-icon-180x180.png" rel="apple-touch-icon" sizes="180x180" />
+    <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/android-chrome-192x192.png" rel="icon" sizes="192x192" type="image/png" />
+    <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/favicon-96x96.png" rel="icon" sizes="96x96" type="image/png" />
+    <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+    <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+    <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/android-chrome-manifest.json" rel="manifest" />
+    <meta content="#f4b528" name="msapplication-TileColor" />
+    <meta content="<?php echo esc_url( get_template_directory_uri() ); ?>/favicons/mstile-144x144.png" name="msapplication-TileImage" />
+    <meta content="#F4B528" name="theme-color" />
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=1">
 
     <link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/build/css/main.css">
